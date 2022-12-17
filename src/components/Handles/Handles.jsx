6da9766d90186f5ea5b080f31812f8f9bf069570
Rishabh Dubey <riishabhdubey8@gmail.com>
2022-12-17 16:37:27 +0530

    second

--- src/components/Handles/Handles.jsx
+++ src/components/Handles/Handles.jsx
@@ -4,7 +4,7 @@ import "./handles.css";
 export default function Handles() {
   return (
     <section className="section--handles">
-      <h2 className="handles--heading">Connect</h2>
+      <h2 className="handles--heading">Connect Me</h2>
       <div className="handles--card-content">
         <i class="fa-brands fa-linkedin-in"></i>
         <h3>linkedin</h3>
